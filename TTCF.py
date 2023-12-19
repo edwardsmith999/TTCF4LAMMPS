49f0ebed75ffa422a44d0eb4c64da3b1faeba9a5
Ed <edward.smith0@brunel.ac.uk>
2023-12-19 14:39:11 +0000

    Refactored into TTCF object which handles all integration and data collection

---
+++ TTCF.py
@@ -0,0 +1,193 @@
+import numpy as np
+
+from utils import *
+
+class TTCF():
+
+    def __init__(self, global_variables, profile_variables, 
+                 Nsteps, Nbins, Nmappings):
+
+        """
+            Create a TTCF object to collect data
+
+            inputs include
+            global_variables - list - variables collected by ave/time
+            profile_variables - list - variables collected by ave/chunk
+            Nsteps - int - Number of steps daughter us run for when TTCF is collected
+            Nbins - int - Number of bins in profiles
+            Nmappings - int - Number of mappings used by duaghters
+        """
+
+        self.global_variables = global_variables
+        self.profile_variables = profile_variables
+        self.Nsteps = Nsteps
+        self.Nbins = Nbins
+        self.Nmappings = Nmappings
+        self.Count = 0
+
+        avetime_ncol = len(global_variables)
+        avechunk_ncol = len(profile_variables) + 2
+
+        #Allocate arrays to store data
+        self.DAV_global_mean  = np.zeros([Nsteps, avetime_ncol])
+        self.DAV_profile_mean   = np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+        self.TTCF_global_mean  = np.zeros([Nsteps, avetime_ncol])
+        self.TTCF_profile_mean   = np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+        self.DAV_global_var  = np.zeros([Nsteps, avetime_ncol])
+        self.DAV_profile_var   = np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+        self.TTCF_global_var  = np.zeros([Nsteps, avetime_ncol])
+        self.TTCF_profile_var   = np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+        self.TTCF_global_partial = np.zeros([Nsteps, avetime_ncol])
+        self.TTCF_profile_partial= np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+        self.DAV_profile_partial= np.zeros([Nsteps, Nbins, avechunk_ncol])
+        self.DAV_global_partial = np.zeros([Nsteps, avetime_ncol])
+
+        self.integrand_global_partial  = np.zeros([Nsteps, avetime_ncol])
+        self.integrand_profile_partial = np.zeros([Nsteps, Nbins, avechunk_ncol])
+
+    def add_mappings(self, data_profile, data_global, omega):
+
+        #Sum the mappings together
+        self.DAV_profile_partial  += data_profile[:,:,:]
+        self.DAV_global_partial   += data_global[:,:]
+        
+        self.integrand_profile_partial += data_profile[:,:,:]*omega
+        self.integrand_global_partial  += data_global[:,:]*omega
+
+    def integrate(self, step):
+
+        #Perform the integration
+        self.TTCF_profile_partial = TTCF_integration(self.integrand_profile_partial, step)
+        self.TTCF_global_partial  = TTCF_integration(self.integrand_global_partial, step)
+
+        #Add the initial value (t=0) 
+        self.TTCF_profile_partial += self.DAV_profile_partial[0,:,:]
+        self.TTCF_global_partial  += self.DAV_global_partial[0,:]
+
+        #Average over the mappings and update the Count (# of children trajectories generated excluding the mappings)
+        self.DAV_profile_partial  /= self.Nmappings   
+        self.DAV_global_partial   /= self.Nmappings 
+        self.TTCF_profile_partial /= self.Nmappings   
+        self.TTCF_global_partial  /= self.Nmappings 
+
+        self.Count += 1
+
+        #Update all means and variances
+        if self.Count >1:
+        
+            self.TTCF_profile_var= update_var(self.TTCF_profile_partial, self.TTCF_profile_mean, self.TTCF_profile_var, self.Count)      
+            self.DAV_profile_var= update_var(self.DAV_profile_partial, self.DAV_profile_mean, self.DAV_profile_var, self.Count)
+            self.TTCF_global_var= update_var(self.TTCF_global_partial, self.TTCF_global_mean, self.TTCF_global_var, self.Count)   
+            self.DAV_global_var= update_var(self.DAV_global_partial, self.DAV_global_mean, self.DAV_global_var, self.Count)
+          
+        self.TTCF_profile_mean= update_mean(self.TTCF_profile_partial, self.TTCF_profile_mean, self.Count)     
+        self.DAV_profile_mean= update_mean(self.DAV_profile_partial, self.DAV_profile_mean, self.Count)
+        self.TTCF_global_mean= update_mean(self.TTCF_global_partial, self.TTCF_global_mean, self.Count)
+        self.DAV_global_mean= update_mean(self.DAV_global_partial, self.DAV_global_mean, self.Count)
+
+        self.DAV_profile_partial[:,:,:] = 0
+        self.DAV_global_partial[:,:]    = 0
+            
+        self.integrand_profile_partial[:,:,:] = 0
+        self.integrand_global_partial[:,:]    = 0
+
+    def finalise_output(self, irank, comm, root=0):
+
+        self.irank = irank
+        self.comm = comm
+        self.root = root
+        self.output_finalised = True
+
+        #Get FINAL COLUMN BECAUSE BY DEFAULT LAMMPS GIVE YOU ALSO THE USELESS INFO ABOUT THE BINS.
+        # For  N QUANTITIES, could TAKE THE LAST N ELEMENTS
+        self.TTCF_profile_mean = self.TTCF_profile_mean[:,:,-1]
+        self.DAV_profile_mean  = self.DAV_profile_mean[:,:,-1]
+
+        self.TTCF_profile_var = self.TTCF_profile_var[:,:,-1]
+        self.DAV_profile_var  = self.DAV_profile_var[:,:,-1]
+
+        self.TTCF_global_var/= float(self.Count)
+        self.DAV_global_var /= float(self.Count)
+        self.TTCF_profile_var /= float(self.Count)
+        self.DAV_profile_var  /= float(self.Count)
+
+        #Compute MEN AND VARIANCE OF BOTH DAV AND TTCF
+        self.TTCF_profile_mean_total = sum_over_MPI(self.TTCF_profile_mean, irank, comm)
+        self.DAV_profile_mean_total = sum_over_MPI(self.DAV_profile_mean, irank, comm)
+        self.TTCF_profile_var_total = sum_over_MPI(self.TTCF_profile_var, irank, comm)
+        self.DAV_profile_var_total = sum_over_MPI(self.DAV_profile_var, irank, comm)
+
+        self.TTCF_global_mean_total = sum_over_MPI(self.TTCF_global_mean, irank, comm)
+        self.DAV_global_mean_total = sum_over_MPI(self.DAV_global_mean, irank, comm)
+        self.TTCF_global_var_total = sum_over_MPI(self.TTCF_global_var, irank, comm)
+        self.DAV_global_var_total = sum_over_MPI(self.DAV_global_var, irank, comm)
+
+        #Total is None on everything but the root processor
+        if irank == self.root:
+            self.TTCF_profile_mean_total = self.TTCF_profile_mean_total/float(self.nprocs)
+            self.DAV_profile_mean_total  = self.DAV_profile_mean_total/float(self.nprocs)
+            self.TTCF_profile_var_total  = self.TTCF_profile_var_total/np.sqrt(self.nprocs)
+            self.DAV_profile_var_total   = self.DAV_profile_var_total/np.sqrt(self.nprocs)
+            
+            self.TTCF_global_mean_total = self.TTCF_global_mean_total/float(self.nprocs)
+            self.DAV_global_mean_total  = self.DAV_global_mean_total/float(self.nprocs)
+            self.TTCF_global_var_total  = self.TTCF_global_var_total/np.sqrt(self.nprocs)
+            self.DAV_global_var_total   = self.DAV_global_var_total/np.sqrt(self.nprocs)
+            
+            self.TTCF_profile_SE_total  = np.sqrt(self.TTCF_profile_var_total)
+            self.DAV_profile_SE_total   = np.sqrt(self.DAV_profile_var_total)
+            self.TTCF_global_SE_total   = np.sqrt(self.TTCF_global_var_total)
+            self.DAV_global_SE_total    = np.sqrt(self.DAV_global_var_total)
+
+
+    def plot_data(self, animated=False):
+
+        if self.output_finalised and self.irank == self.root:
+
+            import matplotlib.pyplot as plt
+
+            if animated:
+                # This code animates the time history
+                plt.ion()
+                fig, ax = plt.subplots(1,1)
+                plt.show()
+                ft = True
+                for t in range(self.TTCF_profile_mean_total.shape[0]):
+                    print(t)
+                    l1, = ax.plot(self.DAV_profile_mean_total[t, :],'r-', label="DAV")
+                    l2, = ax.plot(self.TTCF_profile_mean_total[t, :],'b-', label="TTCF")
+                    if ft:
+                        plt.legend()
+                        ft=False
+                    plt.pause(0.1)
+                    l1.remove()
+                    l2.remove()
+            else:
+                #This code plots the average over time
+                plt.plot(np.mean(self.DAV_profile_mean_total[:, :],0),'r-', label="DAV")
+                plt.plot(np.mean(self.TTCF_profile_mean_total[:, :],0),'b-', label="TTCF")
+                plt.legend()
+                plt.show()
+
+    def save_data(self):
+
+        if self.output_finalised and self.irank == self.root:
+            #Save variables at end of each batch in case of crash
+            np.savetxt('profile_DAV.txt', self.DAV_profile_mean_total)
+            np.savetxt('profile_TTCF.txt', self.TTCF_profile_mean_total)
+            
+            np.savetxt('profile_DAV_SE.txt', self.DAV_profile_SE_total)
+            np.savetxt('profile_TTCF_SE.txt', self.TTCF_profile_SE_total)
+            
+            np.savetxt('global_DAV.txt', self.DAV_global_mean_total)
+            np.savetxt('global_TTCF.txt', self.TTCF_global_mean_total)
+            
+            np.savetxt('global_DAV_SE.txt', self.DAV_global_SE_total)
+            np.savetxt('global_TTCF_SE.txt', self.TTCF_global_SE_total)
+                 
+
--- run_TTCF.py
+++ run_TTCF.py
@@ -1,10 +1,10 @@
 from mpi4py import MPI
 from lammps import lammps
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from utils import *
+from TTCF import TTCF
 
 #This code is run using MPI - each processes will
 #run this same bit code with its own memory
@@ -16,48 +16,42 @@ root = 0
 print("Proc {:d} out of {:d} procs".format(irank+1,nprocs), flush=True)
 
 #Define lengths for all runs, number of Daughters, etc
-
-Tot_Daughters= 1000
-Ndaughters=int(np.ceil(Tot_Daughters/nprocs))
-
-Maps=[0,7,36,35]
-Nmappings=len(Maps)
-
+Tot_Daughters         = 1000
+Maps                  = [0,7,36,35]
 Nsteps_Thermalization = 10000
 Nsteps_Decorrelation  = 10000
 Nsteps_Daughter       = 500
+Delay                 = 10
+Nbins                 = 100
+dt                    = 0.0025
 
-Delay=10
-Nsteps_eff=int(Nsteps_Daughter/Delay)+1
-
-Nbins=100
+#Derived quantities
+Nmappings=len(Maps)
+Ndaughters=int(np.ceil(Tot_Daughters/nprocs))
+Nsteps=int(Nsteps_Daughter/Delay)+1
 Bin_Width=1.0/float(Nbins)
+Thermo_damp = 10*dt
 
-dt = 0.0025
-
-# Here we can define all variables, computes and fixs in lammps format that we want to set for each daughter
+# Here we can define all variables, computes and fixs in lammps 
+#format that we want to set for each daughter
 # and turn off at the end
 setlist = []
-
 # =============== Forces =========================
 #For the case of SLLOD these apply the forces
 setlist.append("variable vx_shear atom vx+${srate}*y")
 setlist.append("set atom * vx v_vx_shear")
 setlist.append("fix box_deform all deform 1 xy erate ${srate} remap v units box")
-setlist.append("fix NVT_SLLOD all nvt/sllod temp ${T} ${T} ${Thermo_damp}")
-
+setlist.append("fix NVT_SLLOD all nvt/sllod temp ${T} ${T} " + str(Thermo_damp))
 # ============= Outputs =========================
 #Here we define all the computes to get outputs from the simulation
-
 #Profile 1D chunks here, can add anything from https://docs.lammps.org/fix_ave_chunk.html
 profile_variables = ['vx']
-
 #Define bin discretization
 setlist.append("compute profile_layers all chunk/atom bin/1d y lower "+str(Bin_Width)+" units reduced")
 #Profile (ave/chunk fix)
 setlist.append("fix Profile_variables all ave/chunk 1 1 {} profile_layers {} ave one".format(Delay, ' '.join(profile_variables)))
 #Define computes to get Omega
-setlist.append("compute		shear_T all temp/deform")
+setlist.append("compute        shear_T all temp/deform")
 setlist.append("compute        shear_P all pressure shear_T ")
 setlist.append("variable       Omega equal -c_shear_P[4]*(xhi-xlo)*(yhi-ylo)*(zhi-zlo)*${srate}/(${k_B}*${T})")
 #And global (ave/time fix) variables, often custom computes/variables, see https://docs.lammps.org/fix_ave_time.html
@@ -66,44 +60,10 @@ global_variables = ['c_shear_P[4]', 'v_Omega']
 setlist.append("fix Global_variables all ave/time 1 1 {} {} ave one".format(Delay, ' '.join(global_variables)))
 
 
-#Allocate empty arrays for data
-avetime_ncol = len(global_variables)
-avechunk_ncol = len(profile_variables) + 2
-
-DAV_global_mean  = np.zeros([Nsteps_eff, avetime_ncol])
-DAV_profile_mean   = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-TTCF_global_mean  = np.zeros([Nsteps_eff, avetime_ncol])
-TTCF_profile_mean   = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-DAV_global_var  = np.zeros([Nsteps_eff, avetime_ncol])
-DAV_profile_var   = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-TTCF_global_var  = np.zeros([Nsteps_eff, avetime_ncol])
-TTCF_profile_var   = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-TTCF_global_partial = np.zeros([Nsteps_eff, avetime_ncol])
-TTCF_profile_partial= np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-DAV_profile_partial= np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-DAV_global_partial = np.zeros([Nsteps_eff, avetime_ncol])
-
-data_global = np.zeros([Nsteps_eff, avetime_ncol])
-data_profile  = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-integrand_global_partial  = np.zeros([Nsteps_eff, avetime_ncol])
-integrand_profile_partial = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-
-#Note a shorthand for this
-#for tp in ['DAV', 'TTCF']:
-#    for a in ['mean', 'var', 'partial']:
-#        d[dp+"_global_"+a] = np.zeros([Nsteps_eff, avetime_ncol])
-#        d[dp+"profile"+a] = np.zeros([Nsteps_eff, Nbins, avechunk_ncol])
-#Which would allow e.g.
-#for tp in ['DAV', 'TTCF']:
-#    for a in ['mean', 'var', 'partial']:
-#       for gp in ['_global_', '_profile_']:
-#            d[dp+gp+a+"_total"] = sum_over_MPI(d[dp+gp+a])
+#Create TTCF class 
+ttcf = TTCF(global_variables, profile_variables, Nsteps, Nbins, Nmappings)
+data_global = np.zeros([Nsteps, len(global_variables)])
+data_profile  = np.zeros([Nsteps, Nbins, len(profile_variables) + 2])
 
 #Create random seed
 if nprocs == 1:
@@ -119,32 +79,26 @@ lmp = lammps(comm=MPI.COMM_SELF, cmdargs=args)
 #Run equilibration  
 lmp.file("system_setup.in")
 lmp.command("timestep " + str(dt))
-lmp.command("variable Thermo_damp equal " +  str(10*dt))
-lmp.command(" fix NVT_thermalization all nvt temp ${T} ${T} ${Thermo_damp} tchain 1")
+lmp.command("fix NVT_thermalization all nvt temp ${T} ${T} " +  str(Thermo_damp) + " tchain 1")
 lmp.command("run " + str(Nsteps_Thermalization))
 lmp.command("unfix NVT_thermalization")
 
 #Save snapshot to use for daughters
 state = save_state(lmp, "snapshot")
 
-Count = 0
-for Nd in range(0,Ndaughters,1):
+#Loop over all sets of daughters
+for Nd in range(Ndaughters):
 
     print("Proc", irank+1, " with daughter =", Nd+1, " of ", Ndaughters,  flush=True)
 
-    #Sampling of the daughters initial state
+    #Run daughter from initial state to decorrelate from mother
     load_state(lmp, state)
-    lmp.command("fix NVT_sampling all nvt temp ${T} ${T} ${Thermo_damp} tchain 1")
+    lmp.command("fix NVT_sampling all nvt temp ${T} ${T} " +  str(Thermo_damp) + " tchain 1")
     lmp.command("run " + str(Nsteps_Decorrelation))
     lmp.command("unfix NVT_sampling")
     state = save_state(lmp, "snapshot")
 
-    DAV_profile_partial[:,:,:] = 0
-    DAV_global_partial[:,:]    = 0
-        
-    integrand_profile_partial[:,:,:] = 0
-    integrand_global_partial[:,:]    = 0
-
+    #Branch off daughters for each mapping
     for Nm in range(Nmappings):
 
         #Load child state
@@ -166,7 +120,7 @@ for Nd in range(0,Ndaughters,1):
         omega = data_global[0, -1] 
 
         #Run over time        
-        for t in range(1 , Nsteps_eff , 1):
+        for t in range(1, Nsteps):
             lmp.command("run " + str(Delay) + " pre yes post no")
             data_profile[t, :, :]= get_fix_data(lmp, "Profile_variables", profile_variables, Nbins)
             data_global[t, :] = get_fix_data(lmp, "Global_variables", global_variables)
@@ -175,123 +129,22 @@ for Nd in range(0,Ndaughters,1):
         unset_list(lmp, setlist)
 
         #Sum the mappings together
-        DAV_profile_partial  += data_profile[:,:,:]
-        DAV_global_partial   += data_global[:,:]
-        
-        integrand_profile_partial += data_profile[:,:,:]*omega
-        integrand_global_partial  += data_global[:,:]*omega
-        
+        ttcf.add_mappings(data_profile, data_global, omega)
+
     #Perform the integration
-    TTCF_profile_partial = TTCF_integration(integrand_profile_partial, dt*Delay)
-    TTCF_global_partial  = TTCF_integration(integrand_global_partial, dt*Delay)
+    ttcf.integrate(dt*Delay)
 
-    #Add the initial value (t=0) 
-    TTCF_profile_partial += DAV_profile_partial[0,:,:]
-    TTCF_global_partial  += DAV_global_partial[0,:]
-
-    #Average over the mappings and update the Count (# of children trajectories generated excluding the mappings)
-    DAV_profile_partial  /= Nmappings   
-    DAV_global_partial   /= Nmappings 
-    TTCF_profile_partial /= Nmappings   
-    TTCF_global_partial  /= Nmappings 
-           
-    Count += 1
-
-    #Update all means and variances
-    if Count >1:
-    
-        TTCF_profile_var= update_var(TTCF_profile_partial, TTCF_profile_mean, TTCF_profile_var, Count)      
-        DAV_profile_var= update_var(DAV_profile_partial, DAV_profile_mean, DAV_profile_var, Count)
-        TTCF_global_var= update_var(TTCF_global_partial, TTCF_global_mean, TTCF_global_var, Count)   
-        DAV_global_var= update_var(DAV_global_partial, DAV_global_mean, DAV_global_var, Count)
-      
-    TTCF_profile_mean= update_mean(TTCF_profile_partial, TTCF_profile_mean, Count)     
-    DAV_profile_mean= update_mean(DAV_profile_partial, DAV_profile_mean, Count)
-    TTCF_global_mean= update_mean(TTCF_global_partial, TTCF_global_mean, Count)
-    DAV_global_mean= update_mean(DAV_global_partial, DAV_global_mean, Count)
-         
-
-
+#Close lammps instance and plot time taken
 lmp.close()
 t2 = MPI.Wtime()
 if irank == root:
     print("Walltime =", t2 - t1, flush=True)
 
-#Get FINAL COLUMN BECAUSE BY DEFAULT LAMMPS GIVE YOU ALSO THE USELESS INFO ABOUT THE BINS.
-# For  N QUANTITIES, could TAKE THE LAST N ELEMENTS
-TTCF_profile_mean = TTCF_profile_mean[:,:,-1]
-DAV_profile_mean  = DAV_profile_mean[:,:,-1]
+#This includes an MPI call so must be used by all processes
+ttcf.finalise_output(irank, comm)
 
-TTCF_profile_var = TTCF_profile_var[:,:,-1]
-DAV_profile_var  = DAV_profile_var[:,:,-1]
-
-TTCF_global_var/= float(Count)
-DAV_global_var /= float(Count)
-TTCF_profile_var /= float(Count)
-DAV_profile_var  /= float(Count)
-
-#Compute MEN AND VARIANCE OF BOTH DAV AND TTCF
-TTCF_profile_mean_total = sum_over_MPI(TTCF_profile_mean, irank, comm)
-DAV_profile_mean_total = sum_over_MPI(DAV_profile_mean, irank, comm)
-TTCF_profile_var_total = sum_over_MPI(TTCF_profile_var, irank, comm)
-DAV_profile_var_total = sum_over_MPI(DAV_profile_var, irank, comm)
-
-TTCF_global_mean_total = sum_over_MPI(TTCF_global_mean, irank, comm)
-DAV_global_mean_total = sum_over_MPI(DAV_global_mean, irank, comm)
-TTCF_global_var_total = sum_over_MPI(TTCF_global_var, irank, comm)
-DAV_global_var_total = sum_over_MPI(DAV_global_var, irank, comm)
-
-#Total is None on everything but the root processor
-if irank == root:
-    TTCF_profile_mean_total = TTCF_profile_mean_total/float(nprocs)
-    DAV_profile_mean_total  = DAV_profile_mean_total/float(nprocs)
-    TTCF_profile_var_total  = TTCF_profile_var_total/np.sqrt(nprocs)
-    DAV_profile_var_total   = DAV_profile_var_total/np.sqrt(nprocs)
-    
-    TTCF_global_mean_total = TTCF_global_mean_total/float(nprocs)
-    DAV_global_mean_total  = DAV_global_mean_total/float(nprocs)
-    TTCF_global_var_total  = TTCF_global_var_total/np.sqrt(nprocs)
-    DAV_global_var_total   = DAV_global_var_total/np.sqrt(nprocs)
-    
-    TTCF_profile_SE_total  = np.sqrt(TTCF_profile_var_total)
-    DAV_profile_SE_total   = np.sqrt(DAV_profile_var_total)
-    TTCF_global_SE_total   = np.sqrt(TTCF_global_var_total)
-    DAV_global_SE_total    = np.sqrt(DAV_global_var_total)
-        
-# This code animates the time history
-#    plt.ion()
-#    fig, ax = plt.subplots(1,1)
-#    plt.show()
-#    ft = True
-#    for t in range(TTCF_profile_mean_total.shape[0]):
-#        print(t)
-#        l1, = ax.plot(DAV_profile_mean_total[t, :],'r-', label="DAV")
-#        l2, = ax.plot(TTCF_profile_mean_total[t, :],'b-', label="TTCF")
-#        if ft:
-#            plt.legend()
-#            ft=False
-#        plt.pause(0.1)
-#        l1.remove()
-#        l2.remove()
-
-    #This code plots the average over time
-    plt.plot(np.mean(DAV_profile_mean_total[:, :],0),'r-', label="DAV")
-    plt.plot(np.mean(TTCF_profile_mean_total[:, :],0),'b-', label="TTCF")
-    plt.legend()
-    plt.show()
-
-    #Save variables at end of each batch in case of crash
-    np.savetxt('profile_DAV.txt', DAV_profile_mean_total)
-    np.savetxt('profile_TTCF.txt', TTCF_profile_mean_total)
-    
-    np.savetxt('profile_DAV_SE.txt', DAV_profile_SE_total)
-    np.savetxt('profile_TTCF_SE.txt', TTCF_profile_SE_total)
-    
-    np.savetxt('global_DAV.txt', DAV_global_mean_total)
-    np.savetxt('global_TTCF.txt', TTCF_global_mean_total)
-    
-    np.savetxt('global_DAV_SE.txt', DAV_global_SE_total)
-    np.savetxt('global_TTCF_SE.txt', TTCF_global_SE_total)
-    
+#Plot and output data
+ttcf.plot_data()
+ttcf.save_data()
 
 MPI.Finalize()
