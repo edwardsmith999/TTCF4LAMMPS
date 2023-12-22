4e741c61727e5e864fcfb1dc1571b90150efcd5c
Ed Smith <edwardsmith999@users.noreply.github.com>
2023-12-22 09:31:07 +0000

    Create setup.py
    Added setup code for Pypi

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup( name = "TTCF4LAMMPS",
+       version = "1.0.0",
+       author = ["Luca Maffioli","Edward Smith"],
+       author_email = "lmaffioli@swin.edu.au",
+       url = "https://github.com/edwardsmith999/TTCF4LAMMPS",
+       classifiers=['Development Status :: 3 - Alpha',
+                     'Programming Language :: Python :: 3.11'],
+       packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+       keywords='Molecular Dynamics TTCF Transient Time Correlation Function',
+       license = "GPL",
+       install_requires=['numpy', 'scipy', 'matplotlib', 'mpi4py', 'pylammps' ],
+       description = "Code to run TTCF function on LAMMPS",
+       long_description = long_description,
+       long_description_content_type='text/markdown',
+       entry_points={
+            'console_scripts': [
+                'TTCF=run_TTCF:main',
+            ],
+       },
+)
