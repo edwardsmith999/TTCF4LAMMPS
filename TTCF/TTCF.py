Added a division by `nprocs` for the correct evaluation of the total variance as average of the variances of every processor.

TTCF/TTCF.py
@@ -133,13 +133,13 @@ class TTCF():
         if irank == self.root:
             self.TTCF_profile_mean_total = self.TTCF_profile_mean_total/float(self.nprocs)
             self.DAV_profile_mean_total  = self.DAV_profile_mean_total/float(self.nprocs)
-            self.TTCF_profile_var_total  = self.TTCF_profile_var_total/float(self.nprocs)
-            self.DAV_profile_var_total   = self.DAV_profile_var_total/float(self.nprocs)
+            self.TTCF_profile_var_total  = self.TTCF_profile_var_total/float(self.nprocs*self.nprocs)
+            self.DAV_profile_var_total   = self.DAV_profile_var_total/float(self.nprocs*self.nprocs)
             
             self.TTCF_global_mean_total = self.TTCF_global_mean_total/float(self.nprocs)
             self.DAV_global_mean_total  = self.DAV_global_mean_total/float(self.nprocs)
-            self.TTCF_global_var_total  = self.TTCF_global_var_total/float(self.nprocs)
-            self.DAV_global_var_total   = self.DAV_global_var_total/float(self.nprocs)
+            self.TTCF_global_var_total  = self.TTCF_global_var_total/float(self.nprocs*self.nprocs)
+            self.DAV_global_var_total   = self.DAV_global_var_total/float(self.nprocs*self.nprocs)
             
             self.TTCF_profile_SE_total  = np.sqrt(self.TTCF_profile_var_total)
             self.DAV_profile_SE_total   = np.sqrt(self.DAV_profile_var_total)
@@ -408,10 +408,10 @@ class TTCFnoMap():
         # Total is None on everything but the root processor
         if irank == self.root:
             self.DAV_profile_mean_total  = self.DAV_profile_mean_total/float(self.nprocs)
-            self.DAV_profile_var_total   = self.DAV_profile_var_total/float(self.nprocs)
+            self.DAV_profile_var_total   = self.DAV_profile_var_total/float(self.nprocs*self.nprocs)
             
             self.DAV_global_mean_total  = self.DAV_global_mean_total/float(self.nprocs)
-            self.DAV_global_var_total   = self.DAV_global_var_total/float(self.nprocs)
+            self.DAV_global_var_total   = self.DAV_global_var_total/float(self.nprocs*self.nprocs)
             
             self.DAV_profile_SE_total   = np.sqrt(self.DAV_profile_var_total)
             self.DAV_global_SE_total    = np.sqrt(self.DAV_global_var_total)